91a383ea8366a1b4091a699a2596ebb3e4904825
Malfeasant <mischasumrall@gmail.com>
2018-02-06 15:55:47 -0700

    Minor shuffling
    But now clock rate is reported as infinity when stepping... different
    than basing it off the single interval, but not better.

--- src/us/malfeasant/admiral64/timing/RunMode.java
+++ src/us/malfeasant/admiral64/timing/RunMode.java
@@ -6,7 +6,6 @@ enum RunMode {
 	STEP("|>") {
 		@Override
 		void setup(TimingGenerator tg) {
-			// deliberately not calling super.setup(tg) as there's no point to reset the calc with every step
 			tg.runFor(1);
 		}
 	},
@@ -14,17 +13,13 @@ enum RunMode {
 		@Override
 		void timerFired(TimingGenerator tg) {
 			long c = (tg.interval * tg.osc.cycles + tg.remainder) / (tg.osc.seconds * 1000000000L);
-			// 17045
 			tg.remainder = (tg.interval * tg.osc.cycles + tg.remainder) % (tg.osc.seconds * 1000000000L);
-			// 4992500000
 			tg.runFor((int)c);
-			//System.out.println(c);
 		}
 	},
 	FAST(">>") {
 		@Override
 		void setup(TimingGenerator tg) {
-			super.setup(tg);
 			tg.runFor(tg.osc.cycles);	// run first batch, further batches will be run after work comes back
 		}
 		@Override
@@ -42,12 +37,12 @@ enum RunMode {
 		b.setOnAction((event) -> tg.setMode(this));
 		return b;
 	}
-	void setup(TimingGenerator tg) {	// called when mode is set, do any necessary preparation
+	void setup(TimingGenerator tg) {}	// called when mode is set, do any necessary preparation
+	void workDone(TimingGenerator tg) {}	// called when worker thread has completed work
+	void timerFired(TimingGenerator tg) {}	// called when animation timer fires
+	void teardown(TimingGenerator tg) {	// called before new mode is set, do any cleanup
 		// reset cycles per second fields
 		tg.elapsed = 0;
 		tg.cyclesDone = 0;
 	}
-	void workDone(TimingGenerator tg) {}	// called when worker thread has completed work
-	void timerFired(TimingGenerator tg) {}	// called when animation timer fires
-	void teardown(TimingGenerator tg) {}	// called before new mode is set, do any cleanup
 }
