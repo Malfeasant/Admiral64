615122fabccd15046fe9ddd49bc07842ca86c689
Malfeasant <mischasumrall@gmail.com>
2018-03-08 17:09:47 -0700

    Added ROMs
    and a couple classes to wrangle them

---
+++ src/us/malfeasant/admiral64/machine/bus/BuiltInROMs.java
@@ -0,0 +1,30 @@
+package us.malfeasant.admiral64.machine.bus;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public enum BuiltInROMs {
+	KERNAL1("kernal.901227-01.bin"),
+	KERNAL2("kernal.901227-02.bin"),
+	KERNAL3("kernal.901227-03.bin"),
+	BASIC("basic.901226-01.bin"),
+	CHAR("characters.901225-01.bin", 12);
+	
+	private final String filename;
+	private final int abits;
+	
+	private BuiltInROMs(String fn, int b) {
+		filename = fn;
+		abits = b;
+	}
+	private BuiltInROMs(String fn) {
+		this(fn, 13);
+	}
+	
+	public ROM load() throws IOException {
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(filename);
+		byte[] bytes = new byte[2 ^ abits];
+		is.read(bytes);
+		return new ROM(abits, bytes);
+	}
+}
