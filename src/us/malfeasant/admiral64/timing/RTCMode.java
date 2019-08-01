151353aaf5b6c4ed521bdb791ee4eeb7a224ada6
Malfeasant <mischasumrall@gmail.com>
2019-08-01 16:59:54 -0700

    Nothing substantial
    Too much work, not enough time...

--- src/us/malfeasant/admiral64/timing/RTCMode.java
+++ src/us/malfeasant/admiral64/timing/RTCMode.java
@@ -1,13 +1,14 @@
 package us.malfeasant.admiral64.timing;
 
 /**
- * Represents the different ways the RTC can be handled- note, there are actually two RTCs, one in each CIA chip.
+ * Represents the different ways the RTC can be handled- note, there are actually two RTCs, one in each CIA chip.  Maybe let
+ * each have its mode set separately?
  * Fully simulated- clock runs in sync w/ Oscillator, if paused/stepped RTC slows as well- time can be set from within simulation
- * Partial realtime- time can be set in sim, but this is treated as offset- passage of time happens in realtime
+ * Partial realtime- time can be set in sim, but this is treated as offset- passage of time happens in realtime regardless of sim speed
  * Full realtime- no setting time from within sim, read of clock reflects host local time no matter what
  * (with the exception of latching)
  * @author Malfeasant
  */
 public enum RTCMode {
-	
+	SIM, OFFSET, REALTIME;
 }
