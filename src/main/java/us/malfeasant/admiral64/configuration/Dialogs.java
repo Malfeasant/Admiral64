6badce1e0cab0abdbffebb02b54bb4f68ea0cb22
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2023-04-19 23:03:28 -0700

    Make edit & create share their common bits

--- src/main/java/us/malfeasant/admiral64/configuration/Dialogs.java
+++ src/main/java/us/malfeasant/admiral64/configuration/Dialogs.java
@@ -8,50 +8,96 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Dialog;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.layout.GridPane;
 
+/**
+ * Helper class- static methods to build, display, and return input from dialogs
+ */
 public class Dialogs {
+
+    /**
+     * Displays a (very primative) dialog to setup a new machine with default options
+     * 
+     * @return a new Configuration object reflecting user's choices
+     */
     public static Optional<Configuration> showCreateDialog() {
-        var dialog = new Dialog<Configuration>();
-        dialog.setTitle("Build new machine");
-        var createType = new ButtonType("Create", ButtonData.OK_DONE);
-        dialog.getDialogPane().getButtonTypes().addAll(createType, ButtonType.CANCEL);
-        var pane = new GridPane();
-        var nameField = new TextField();
-        Platform.runLater(() -> nameField.requestFocus());
-        nameField.setPromptText("Enter machine name:");
-        pane.add(nameField, 0, 0);
-
-        var oscBox = new ChoiceBox<Oscillator>();
-        oscBox.getItems().addAll(Oscillator.values());
-        oscBox.getSelectionModel().clearAndSelect(0);
-        pane.add(oscBox, 1, 0);
-
-        var powBox = new ChoiceBox<Power>();
-        powBox.getItems().addAll(Power.values());
-        powBox.getSelectionModel().clearAndSelect(0);
-        pane.add(powBox, 2, 0);
-
-        var builder = new ConfigurationBuilder();
-        builder.nameProperty.bind(nameField.textProperty());
-        builder.oscillatorProperty.bind(oscBox.valueProperty());
-        builder.powerProperty.bind(powBox.valueProperty());
-
-        dialog.getDialogPane().setContent(pane);
-
-        dialog.setResultConverter(buttonType -> {
-            if (buttonType == createType) {
-                Logger.debug("Creating new machine named " + builder.nameProperty.get() +
-                    " with Oscillator " + builder.oscillatorProperty.get() +
-                    " and Power " + builder.powerProperty.get());
-                return builder.makeFrom();
-            }
-            Logger.debug("Looks like user cancelled machine creation...");
-            return null;
-        });
-        return dialog.showAndWait();
+        return showDialog(null);
     }
 
-    private Dialogs() {} // only exists for static methods
+    /**
+     * Displays a (very primative) dialog to edit an existing machine
+     * 
+     * @param currentConfiguration an existing object to preload the dialog
+     * @return a new Configuration object reflecting user's choices
+     */
+    public static Optional<Configuration> showEditDialog(Configuration currentConfiguration) {
+        return showDialog(currentConfiguration);
+    }
+
+    /**
+     * Nifty way to make a lazily initialized singleton object without
+     * having to worry about any concurrency gotchas
+     */
+    private static enum Singleton {
+        INSTANCE;
+
+        private final Dialog<Configuration> dialog;
+        private final TextField nameField;
+        private final ChoiceBox<Oscillator> oscBox;
+        private final ChoiceBox<Power> powBox;
+        private ConfigurationBuilder builder;
+
+        Singleton() {
+            dialog = new Dialog<>();
+            nameField = new TextField();
+            nameField.setPromptText("Machine name:");
+            oscBox = new ChoiceBox<Oscillator>();
+            oscBox.getItems().addAll(Oscillator.values());
+            powBox = new ChoiceBox<Power>();
+            powBox.getItems().addAll(Power.values());
+
+            var pane = new GridPane();
+            pane.add(nameField, 0, 0);
+            pane.add(oscBox, 1, 0);
+            pane.add(powBox, 2, 0);
+            dialog.getDialogPane().getButtonTypes().addAll(ButtonType.APPLY, ButtonType.CANCEL);
+            dialog.getDialogPane().setContent(pane);
+            dialog.setResultConverter(type -> {
+                if (type == ButtonType.APPLY) {
+                    Logger.debug("Creating new machine named " + builder.nameProperty.get() +
+                        " with Oscillator " + builder.oscillatorProperty.get() +
+                        " and Power " + builder.powerProperty.get());
+                    return builder.makeFrom();
+                }
+                Logger.debug("Looks like user cancelled machine creation...");
+                return null;
+            });
+        }
+    }
+
+    private static Optional<Configuration> showDialog(Configuration oldConfiguration) {
+        var I = Singleton.INSTANCE; // shorthand...
+        if (oldConfiguration == null) { // if nothing passed in, build a new one
+            I.dialog.setTitle("Build new machine");
+            I.nameField.setText("");
+            I.oscBox.getSelectionModel().clearAndSelect(0); // set a default
+            I.powBox.getSelectionModel().clearAndSelect(0); // otherwise nothing is selected
+        } else {
+            I.dialog.setTitle("Edit machine");
+            I.nameField.setText(oldConfiguration.name);
+            I.oscBox.getSelectionModel().clearAndSelect(oldConfiguration.oscillator.ordinal());
+            I.powBox.getSelectionModel().clearAndSelect(oldConfiguration.power.ordinal());
+        }
+
+        Platform.runLater(() -> I.nameField.requestFocus());
+        
+        I.builder = new ConfigurationBuilder();
+        I.builder.nameProperty.bind(I.nameField.textProperty());
+        I.builder.oscillatorProperty.bind(I.oscBox.valueProperty());
+        I.builder.powerProperty.bind(I.powBox.valueProperty());
+
+        return I.dialog.showAndWait();
+    }
+
+    private Dialogs() {} // class only exists for its static methods
 }
