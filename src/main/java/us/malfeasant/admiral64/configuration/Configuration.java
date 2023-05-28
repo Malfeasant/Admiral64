bb9603e1ef7ee4a134bde5007022661ec5126c23
Malfeasant <1212508+Malfeasant@users.noreply.github.com>
2023-05-28 13:52:12 -0700

    Had an idea, then thought better. Left comment to summarize.

--- src/main/java/us/malfeasant/admiral64/configuration/Configuration.java
+++ src/main/java/us/malfeasant/admiral64/configuration/Configuration.java
@@ -2,6 +2,14 @@ package us.malfeasant.admiral64.configuration;
 
 import org.tinylog.Logger;
 
+/**
+ * Houses all the configuration bits that are *not* expected to change- chipset, pal vs ntsc,
+ * chip revisions, etc.  What should *not* be in here is stuff that is trivial to change while
+ * the machine is running- disk images, simulation parameters...
+ * So for example, whether the CIA chips' ToD clock is simulated or returns real time should
+ * not be in here. (yes I started to, then deciced against it) Now, should 60 vs 50 Hz be here?
+ * Leaving it for the moment, but since there is no actual difference to the machine, maybe not?
+ */
 public class Configuration {
     public final String name;
     public final Oscillator oscillator;
@@ -9,7 +17,7 @@ public class Configuration {
 
     Configuration(String name, Oscillator osc, Power pow) {
         if (name == null || osc == null || pow == null) {
-            Logger.error(new IllegalArgumentException("Nulls in Configuration constructor!"));
+            Logger.error(new IllegalArgumentException("Null(s) in Configuration constructor!"));
         }
         this.name = name;
         oscillator = osc;
